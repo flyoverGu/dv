2b33d094398e5b89810e7a6a9cb6769740d7137d
flyover <flyover.gu@gmail.com>
2017-03-18 22:35:01 +0800

    f

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
 ;(function() {
     function getOneUrl($video, done) {
-        function foo($video, done) {
-            var src = $video.find('video').attr('src');
-            if (src) {
-                $video.find('.hv-ring').click();
-                done(src, $video);
-            } else {
-                setTimeout(function() {
-                    foo($video, done);
-                }, 100);
-            }
-        }
-        $video.find('.hv-ring').click();
-        foo($video, done);
     }
 
     function getDom(url) {
-        var dom = $('<a href="' + url + '" download style=" position: absolute; z-index: 99999; color: #fff; background: rgba(0, 0, 0, 0.60); font-size: 16px; bottom: 0px; right: 0px; padding: 3px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; ">下载视频</a>');
+        var dom = $('<a href="' + url + '" download style=" position: absolute; z-index: 999; color: #fff; background: rgba(0, 0, 0, 0.60); font-size: 16px; bottom: 0px; right: 0px; padding: 3px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; ">下载视频</a>');
         if (navigator.userAgent.indexOf("MSIE") != -1) {
             dom.html('下载地址(右键另存为)');
         }
         return dom;
     }
 
-    function getUrl() {
-        var $videoList = $('.html5-video');
-        for (var i = 0; i < $videoList.length; i++) {
-            getOneUrl($videoList.eq(i), function(url, $video) {
-                if (!/\/tv\//.test(document.location.pathname)) {
-                    $video.parent().css({position: 'relative'});
-                }
-                $video.after(getDom(url));
-            });
+    function parseVideoUrl(str) {
+        var list = decodeURIComponent(str).split('&');
+        //console.log(list);
+        var obj = {};
+        for(var i = 0; i < list.length; i++) {
+            var m = list[i].split('=');
+            if (m[0] == 'video_src') {
+                obj['video_src'] = m[1]+'='+m[2];
+            } else if (m[0] == 'Expires') {
+                obj['Expires'] = list[i];
+            } else if (m[0] == 'ssig') {
+                obj['ssig'] = list[i];
+            } else if (m[0] == 'KID') {
+                obj['KID'] = list[i];
+            }
         }
+        //console.log(obj);
+        return obj['video_src']+'&'+obj['Expires']+'&'+obj['ssig']+'&'+obj['KID'];
+    }
+
+    function isTvDomain() {
+        return /\/tv\//.test(document.location.pathname);
+    }
+
+    function appendBtn($el, url) {
+        var dom = getDom(url);
+        if (!isTvDomain()) {
+            $el.css({position: 'relative'});
+        }
+        $el.append(dom);
+    }
+
+    function findAllVideoCard(done) {
+        var videoCardList = $('.WB_video');
+        if (isTvDomain()) {
+            videoCardList = $('[node-type="common_video_player"]');
+        }
+        videoCardList.map(function() {
+            var $el = $(this);
+            var url = parseVideoUrl($el.attr('action-data'));
+            //console.log(url);
+            done($el, url);
+        });
     }
 
     function isSupportDomain() {
@@ -43,14 +63,20 @@
         return true;
     }
 
+    function loadScript(done) {
+        var node = document.createElement('script');
+        node.src = '//cdn.bootcss.com/zepto/1.2.0/zepto.min.js';
+        node.onload = function() {
+            done();
+        }
+        document.body.appendChild(node);
+    }
+
     function start() {
         if (isSupportDomain()) {
-            var node = document.createElement('script');
-            node.src = '//cdn.bootcss.com/zepto/1.2.0/zepto.min.js';
-            node.onload = function() {
-                getUrl();
-            }
-            document.body.appendChild(node);
+            loadScript(function() {
+                findAllVideoCard(appendBtn);
+            });
         }
     }
 
