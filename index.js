dca12b2428ad0f42375af7fb392854112a0b1083
flyover <flyover.gu@gmail.com>
2017-03-19 12:53:37 +0800

    f

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 ;(function() {
-    function getOneUrl($video, done) {
-    }
 
     function getDom(url) {
-        var dom = $('<a href="' + url + '" download style=" position: absolute; z-index: 999; color: #fff; background: rgba(0, 0, 0, 0.60); font-size: 16px; bottom: 0px; right: 0px; padding: 3px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; ">下载视频</a>');
-        if (navigator.userAgent.indexOf("MSIE") != -1) {
+        var dom = $('<a href="' + url + '" download="1.mp4" style=" position: absolute; z-index: 999; color: #fff; background: rgba(0, 0, 0, 0.60); font-size: 16px; bottom: 0px; right: 0px; padding: 3px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; ">下载视频</a>');
+        if (navigator.userAgent.indexOf("MSIE") != -1 || navigator.userAgent.indexOf("Safari") != -1) {
             dom.html('下载地址(右键另存为)');
         }
         return dom;
@@ -34,6 +32,10 @@
         return /\/tv\//.test(document.location.pathname);
     }
 
+    function isSearchDomain() {
+        return /^s\.weibo\.com/.test(document.location.host);
+    }
+
     function appendBtn($el, url) {
         var dom = getDom(url);
         if (!isTvDomain()) {
@@ -50,7 +52,6 @@
         videoCardList.map(function() {
             var $el = $(this);
             var url = parseVideoUrl($el.attr('action-data'));
-            //console.log(url);
             done($el, url);
         });
     }
