a36908249ce024388d7f15eb15b5b31c6c799ae8
flyover <flyover.gu@gmail.com>
2017-03-19 20:28:34 +0800

    f

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
     function getDom(url) {
         var dom = $('<a href="' + url + '" download="1.mp4" style=" position: absolute; z-index: 999; color: #fff; background: rgba(0, 0, 0, 0.60); font-size: 16px; bottom: 0px; right: 0px; padding: 3px; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; ">下载视频</a>');
-        if (navigator.userAgent.indexOf("MSIE") != -1 || navigator.userAgent.indexOf("Safari") != -1) {
+        if (navigator.userAgent.indexOf("MSIE") != -1) {
             dom.html('下载地址(右键另存为)');
         }
         return dom;
